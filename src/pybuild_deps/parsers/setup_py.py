29769ed094acf495f2e1a739055b5876ddbb7dce
Bruno Ciconelle <bciconel@redhat.com>
2023-01-26 16:03:55 -0300

    feat: Build dependency parser for setup.py
    Signed-off-by: Bruno Ciconelle <bciconel@redhat.com>

---
+++ src/pybuild_deps/parsers/setup_py.py
@@ -0,0 +1,85 @@
+"""Build depenencies parser for setup.py files."""
+
+from __future__ import annotations
+
+import ast
+import operator
+from contextlib import suppress
+
+
+def attrgetter(attr, obj):
+    """Get attribute from object.
+
+    Get attribute(s) acceppting a dotted notation.
+    Returns None if attr is not found.
+    """
+    with suppress(AttributeError):
+        return operator.attrgetter(attr)(obj)
+
+
+def parse_setup_py(content: str):
+    """Parse build dependencies from setup.py file.
+
+    It attempts to cover more conventional use cases only.
+    See its tests to have a better idea of what's supported.
+    """
+    code_tree = ast.parse(content)
+    setup_expr = _get_setup_expr(code_tree)
+    if setup_expr is None:
+        # module don't have a setup/setuptools.setup
+        return []
+
+    setup_requires_ast = _get_setup_requires(setup_expr)
+    return _resolve_deps(setup_requires_ast, code_tree)
+
+
+def _get_setup_expr(module: ast.Module) -> ast.Expr | None:
+    for elem in module.body:
+        if isinstance(elem, ast.Expr) and isinstance(
+            attrgetter("value", elem), ast.Call
+        ):
+            func_id = attrgetter("func.id", elem.value)
+            if func_id == "setup":
+                return elem
+            func_attr = attrgetter("func.attr", elem.value)
+            func_value_id = attrgetter("func.value.id", elem.value)
+            if func_attr == "setup" and func_value_id == "setuptools":
+                return elem
+
+
+def _get_setup_requires(setup_expr: ast.Expr):
+    keywords = attrgetter("value.keywords", setup_expr) or []
+    for kw in keywords:
+        if kw.arg == "setup_requires":
+            return kw.value
+
+
+def _resolve_name(name: ast.Name, module: ast.Module):
+    for elem in module.body:
+        if isinstance(elem, ast.Assign):
+            if not (len(elem.targets) == 1 and isinstance(elem.targets[0], ast.Name)):
+                raise NotImplementedError()
+            if elem.targets[0].id == name.id:
+                if isinstance(elem.value, ast.Name):
+                    return _resolve_name(elem.value, module)
+                return elem.value
+
+
+def _ast_value_getter(element, module: ast.Module):
+    if isinstance(element, ast.Constant):
+        return element.value
+    elif isinstance(element, ast.Name):
+        target_element = _resolve_name(element, module)
+        return _ast_value_getter(target_element, module)
+    raise NotImplementedError()
+
+
+def _resolve_deps(deps_ast, module: ast.Module):
+    if deps_ast is None:
+        return []
+    if isinstance(deps_ast, ast.List):
+        return [_ast_value_getter(el, module) for el in deps_ast.elts]
+    elif isinstance(deps_ast, ast.Name):
+        value = _resolve_name(deps_ast, module)
+        return _resolve_deps(value, module)
+    raise NotImplementedError()
---
+++ tests/test_parsers/test_setup_py.py
@@ -0,0 +1,172 @@
+"""Tests for setup.py parser."""
+
+import pytest
+
+from pybuild_deps.parsers import parse_setup_py
+
+SIMPLE = """
+from setuptools import setup
+
+setup()
+"""
+
+SIMPLE_SETUPTOOLS = """
+import setuptools
+
+setuptools.setup()
+"""
+
+SIMPLE_NON_SETUPTOOLS = """
+import s
+
+def whatever(**kwargs):
+    ...
+
+s.setup()
+whatever(foo="bar")
+"""
+
+SIMPLE_NO_SETUP_REQUIRES = """
+from setuptools import setup
+
+setup(foo="bar")
+"""
+
+DIRECT = """
+from setuptools import setup
+
+setup(
+    "some arg",
+    some_kw="kw",
+    setup_requires=[
+        "foo",  # comments should not be a problem
+    ],
+)
+"""
+
+DIRECT_SETUPTOOLS = """
+import setuptools
+
+setuptools.setup(
+    setup_requires=["foo"]
+)
+"""
+
+USING_CONSTANT = """
+from setuptools import setup
+
+SETUP_REQUIRES = [
+    "bar",  # some comment
+]
+
+setup(
+    setup_requires=SETUP_REQUIRES,
+)
+"""
+
+NO_SETUP = """
+def foo():
+    ...
+def bar():
+    ...
+
+foo()
+bar()
+"""
+
+INDIRECT = """
+from setuptools import setup
+DEP = "baz"
+SETUP_REQUIRES = [DEP]
+setup(setup_requires=SETUP_REQUIRES)
+"""
+
+INDIRECT2 = """
+from setuptools import setup
+BAZ = "baz"
+DEP = BAZ
+SETUP_REQUIRES = [DEP]
+setup(setup_requires=SETUP_REQUIRES)
+"""
+
+IMPORTED_DEPENDENCY = """
+from setuptools import setup
+from foo import bar
+setup(setup_requires=bar)
+"""
+
+KWARGS_DICT = """
+from setuptools import setup
+
+kwargs = dict(setup_requires="foo")
+setup(**kwargs)
+"""
+KWARGS_DICT_LITERAL = """
+from setuptools import setup
+
+kwargs = {"setup_requires": "foo"}
+setup(**kwargs)
+"""
+
+MULTIPLE_ASSIGNMENT = """
+from setuptools import setup
+
+# not sure why someone would do it like this
+SETUP_REQUIRES, OTHER_CONSTANT = ["foo"], "bar"
+
+setup(
+    setup_requires=SETUP_REQUIRES,
+)
+"""
+
+MULTIPLE_ASSIGNMENT_2 = """
+from setuptools import setup
+
+# yet another weird unsupported scenario
+DEP1, DEP2 = ["foo", "bar"]
+
+setup(
+    setup_requires=[DEP1, DEP2],
+)
+"""
+
+
+@pytest.mark.parametrize(
+    "setup_py,expected_result",
+    [
+        (SIMPLE, []),
+        (SIMPLE_NO_SETUP_REQUIRES, []),
+        (SIMPLE_NON_SETUPTOOLS, []),
+        (USING_CONSTANT, ["bar"]),
+        (DIRECT, ["foo"]),
+        (IMPORTED_DEPENDENCY, []),
+        (INDIRECT, ["baz"]),
+        (INDIRECT2, ["baz"]),
+        (NO_SETUP, []),
+        (SIMPLE_SETUPTOOLS, []),
+        (DIRECT_SETUPTOOLS, ["foo"]),
+        pytest.param(
+            KWARGS_DICT,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+        pytest.param(
+            KWARGS_DICT_LITERAL,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+        pytest.param(
+            MULTIPLE_ASSIGNMENT,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+        pytest.param(
+            MULTIPLE_ASSIGNMENT_2,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+    ],
+)
+def test_parse_setup_py(setup_py, expected_result):
+    """Test setup.py parser."""
+    assert parse_setup_py(setup_py) == expected_result
