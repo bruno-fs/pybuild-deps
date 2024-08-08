c4023c5626b7ef4dd052e63146d1788915e3bb1f
Bruno FS Ciconelle <bciconel@redhat.com>
2024-08-08 19:11:50 -0300

    feat: resolve dependencies independently

--- src/pybuild_deps/compile_build_dependencies.py
+++ src/pybuild_deps/compile_build_dependencies.py
@@ -19,6 +19,7 @@ from piptools.resolver import BacktrackingResolver
 
 from .exceptions import UnsolvableDependenciesError
 from .finder import find_build_dependencies
+from .logger import log
 from .utils import get_version
 
 
@@ -28,46 +29,77 @@ class BuildDependencyCompiler:
     def __init__(self, repository: PyPIRepository) -> None:
         self.repository = repository
         self.resolver = None
+        self.dependency_cache = {}
 
     def resolve(
         self,
         install_requirements: Iterable[InstallRequirement],
-        constraints: Iterable[InstallRequirement] | None = None,
     ) -> set[InstallRequirement]:
         """Resolve all build dependencies for a given set of dependencies."""
-        constraints: list[InstallRequirement] = list(constraints) if constraints else []
-        constraint_qty = len(constraints)
-        for req in install_requirements:
-            req_version = get_version(req)
+        all_build_deps = []
+        for ireq in install_requirements:
+            log.info("=" * 80)
+            log.info(str(ireq))
+            log.info("-" * 80)
+            req_version = get_version(ireq)
+            version_tuple = ireq.name, req_version
+            if version_tuple in self.dependency_cache:
+                all_build_deps.extend(self.dependency_cache[version_tuple])
+                log.debug(f"{ireq} exists in our cache, moving on...")
+                continue
             raw_build_dependencies = find_build_dependencies(
-                req.name, req_version, raise_setuppy_parsing_exc=False
+                ireq.name, req_version, raise_setuppy_parsing_exc=False
             )
+            if not raw_build_dependencies:
+                self.dependency_cache[version_tuple] = set()
+                continue
+
+            constraints = []
             for raw_build_req in raw_build_dependencies:
                 build_req = install_req_from_req_string(
-                    raw_build_req, comes_from=req.name
+                    raw_build_req, comes_from=ireq.name
                 )
                 constraints.append(build_req)
-        # override resolver - we only want the latest and greatest
-        self.resolver = BacktrackingResolver(
-            constraints=constraints,
-            existing_constraints={},
-            repository=self.repository,
-            allow_unsafe=True,
-        )
-        try:
-            build_dependencies = self.resolver.resolve()
-        except DistributionNotFound as err:
-            if isinstance(err.__cause__, ResolutionImpossible):  # pragma: no cover
-                unsolvable_deps = err.__cause__.args
-                raise UnsolvableDependenciesError(  # noqa: B904
-                    unsolvable_deps, constraints
-                )
-            raise err
-        # dependencies of build dependencies might have their own build dependencies,
-        # so let's recursively search for those.
-        while len(build_dependencies) != constraint_qty:
-            constraint_qty = len(build_dependencies)
-            build_dependencies = self.resolve(
-                build_dependencies, constraints=build_dependencies
+            # override resolver - we only want the latest and greatest
+            self.resolver = BacktrackingResolver(
+                constraints=constraints,
+                existing_constraints={},
+                repository=self.repository,
+                allow_unsafe=True,
             )
-        return build_dependencies
+            try:
+                build_dependencies = self.resolver.resolve()
+            except DistributionNotFound as err:
+                if isinstance(err.__cause__, ResolutionImpossible):  # pragma: no cover
+                    unsolvable_deps = err.__cause__.args
+                    raise UnsolvableDependenciesError(  # noqa: B904
+                        unsolvable_deps, constraints
+                    )
+                raise err
+            # dependencies of build dependencies might have their own build
+            # dependencies, so let's recursively search for those.
+            build_deps_qty = 0
+            while len(build_dependencies) != build_deps_qty:
+                build_deps_qty = len(build_dependencies)
+                build_dependencies |= set(self.resolve(build_dependencies))
+            self.dependency_cache[version_tuple] = build_dependencies
+            all_build_deps.extend(build_dependencies)
+        return deduplicate_install_requirements(all_build_deps)
+
+
+def deduplicate_install_requirements(ireqs: Iterable[InstallRequirement]):
+    """Deduplicate InstallRequirements."""
+    unique_ireqs = {}
+    for ireq in ireqs:
+        version_tuple = ireq.name, get_version(ireq)
+        if version_tuple not in unique_ireqs:
+            # NOTE: piptools hacks pip's InstallRequirement to allow support from
+            # multiple sources. Let's use the same attr so piptools file writer can
+            # use this information.
+            # https://github.com/jazzband/pip-tools/blob/53309647980e2a4981db54c0033f98c61142de0b/piptools/resolver.py#L118-L122
+            # https://github.com/jazzband/pip-tools/blob/53309647980e2a4981db54c0033f98c61142de0b/piptools/writer.py#L309-L314
+            ireq._source_ireqs = getattr(ireq, "_source_ireqs", [ireq])
+            unique_ireqs[version_tuple] = ireq
+        else:
+            unique_ireqs[version_tuple]._source_ireqs.append(ireq)
+    return set(unique_ireqs.values())
